b59e8bcf7ab20b525ed9ada2c898bc9ff0b8f477
akankshaThalner <165530467+akankshaThalner@users.noreply.github.com>
2024-03-31 07:46:10 +0000

    conversionOperation

---
+++ 01_basic/04_conversionOperation.js
@@ -0,0 +1,19 @@
+// node 01_basic/04_conversionOperation.js
+
+
+console.log(2>1);//true
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2!=1);
+//when you conpare data type should same 
+console.log("2">1);//conver into 2
+console.log("02">1);//same
+
+//convert
+console.log(null > 0);// 0 > 0
+console.log(null == 0);// 0 == 0
+console.log(null >= 0);// true -> not given predictable result 
+
+
+console.log(undefined == 0);
