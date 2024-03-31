bb0e980395320c15e89cba99c71439bb252412b7
akankshaThalner <165530467+akankshaThalner@users.noreply.github.com>
2024-03-31 06:12:10 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version
+
+
+// alert(3+3)//we are using nodejs,not brower
+console.log(3+3);
+
+let name ="hitesh"
+let age = 18;
+let isLoggedIn = false
+
+// primitive data type
+
+ // number => 2 to power 53
+//  bigint
+// string =.""
+//boolean => true/false
+//null standalone value // representation of empty values
+//undefined  // let state;
+//symbol   //using to identify uniquenees  which type of cmponents 
+
+
+//object
+
+console.log(typeof "akshu");//string
+
+console.log(typeof null);//object
+
+console.log(typeof undefined);//undefined   // b'z its a type 
