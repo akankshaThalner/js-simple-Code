fc060722d85f68715d2f9d86ec536c1a1fb29b68
akankshaThalner <165530467+akankshaThalner@users.noreply.github.com>
2024-03-31 09:28:41 +0000

    conversionOperation.

--- 01_basic/04_conversionOperation.js
+++ 01_basic/04_conversionOperation.js
@@ -7,8 +7,8 @@ console.log(2<1);
 console.log(2<=1);
 console.log(2!=1);
 //when you conpare data type should same 
-console.log("2">1);//conver into 2
-console.log("02">1);//same
+console.log("2">1);//conver into 2 true
+console.log("02">1);//same true
 
 //convert
 console.log(null > 0);// 0 > 0
@@ -16,4 +16,19 @@ console.log(null == 0);// 0 == 0
 console.log(null >= 0);// true -> not given predictable result 
 
 
+console.log(undefined > 0);
 console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+/*
+if we are check "2"==2 first it will perform convertion 
+then give result(check anly value )
+
+
+if we are check "2"===2 then it will check 
+dtatatype then value(check data type then value)
+*/
+
+console.log("2"==2)//false
+console.log("2"===2)//false
